e487414b55d62bb92c6335eac8ffa93abc42302f
Meng Bo <bmeng@redhat.com>
2018-04-17 14:47:39 +0800

    fix: remove the old images before pull

--- pull_images.sh
+++ pull_images.sh
@@ -1,15 +1,13 @@
 #/bin/bash
 image_version=`curl -s $REPO_URL | grep -oE 'atomic-openshift-3.[0-9]{2}.[0-9]{1,3}[,-][0-9]{1,3}.[0-9]{1,3}.[0-9]' | cut -d- -f3- | uniq`
 image_tag=v${image_version}.0
-
+main_version=v$(echo $image_version | cut -d- -f1)
 old_image_version=`cat /tmp/image_version`
 
 if [[ $image_version != $old_image_version ]]
 then 
+    ssh bmeng@fedorabmeng.usersys.redhat.com "sudo docker rmi -f $(sudo docker images | grep "$main_version" |awk '{print $3}' | uniq)"
     ssh bmeng@fedorabmeng.usersys.redhat.com "sync_images $image_tag brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/openshift3"
-    if [ $? -eq 0 ]
-        then
-        touch /tmp/image_version
-    	echo -e $image_version > /tmp/image_version
-    fi
+    touch /tmp/image_version
+    echo -e $image_version > /tmp/image_version
 fi
